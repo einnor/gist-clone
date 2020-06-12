48af9d3dbfa0ad42829f285adf8035421773395c
Ronnie Nyaga <ronnienyaga@gmail.com>
2020-06-12 20:13:44 +0300

    Transition animation

--- app/controllers/index.js
+++ app/controllers/index.js
@@ -2,6 +2,8 @@ import Controller from '@ember/controller';
 import { inject as service } from '@ember/service';
 import { action } from '@ember/object';
 import { tracked } from '@glimmer/tracking';
+import { fadeOut, fadeIn } from 'ember-animated/motions/opacity';
+import move from 'ember-animated/motions/move';
 
 export default class IndexController extends Controller {
   @service gistsInfo;
@@ -18,5 +20,23 @@ export default class IndexController extends Controller {
 
   @action snap () {
     this.snapped = !this.snapped;
+  }
+
+  transition = function * ({ insertedSprites, keptSprites, removedSprites }) {
+    for (let sprite of insertedSprites) {
+      sprite.startAtPixel({ x: window.innerWidth });
+      fadeIn(sprite);
+      move(sprite);
+    }
+
+    for (let sprite of keptSprites) {
+      move(sprite);
+    }
+
+    for (let sprite of removedSprites) {
+      sprite.endAtPixel({ x: window.innerWidth });
+      fadeOut(sprite);
+      move(sprite);
+    }
   }
 }
