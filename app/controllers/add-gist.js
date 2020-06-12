f6b17e447784bb207ee5a66f24c56d627840bcd9
Ronnie Nyaga <ronnienyaga@gmail.com>
2020-06-12 19:32:29 +0300

    Refactor

--- app/controllers/add-gist.js
+++ app/controllers/add-gist.js
@@ -1,22 +1,4 @@
 import Controller from '@ember/controller';
 
 export default class AddGistController extends Controller {
-  gists = [
-    {
-      fileName: 'example.hbs',
-      description: 'Just an example',
-      body: '<p>An <strong>example</strong> for {{username}}</p>'
-    },
-    {
-      fileName: 'another-example.js',
-      description: 'Some JS',
-      body: `
-        import Component from '@glimmer/component'
-
-        export default class MyComponent extends Component {
-          variable = 'value';
-        }
-      `
-    },
-  ];
 }
--- app/services/gists-info.js
+++ app/services/gists-info.js
@@ -1,4 +1,22 @@
 import Service from '@ember/service';
 
 export default class GistsInfoService extends Service {
+  gists = [
+    {
+      fileName: 'example.hbs',
+      description: 'Just an example',
+      body: '<p>An <strong>example</strong> for {{username}}</p>'
+    },
+    {
+      fileName: 'another-example.js',
+      description: 'Some JS',
+      body: `
+        import Component from '@glimmer/component'
+
+        export default class MyComponent extends Component {
+          variable = 'value';
+        }
+      `
+    },
+  ];
 }
