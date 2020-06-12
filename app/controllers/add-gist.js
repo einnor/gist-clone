b51d1710a4fee792686f4953923d5f81d7e38c8e
Ronnie Nyaga <ronnienyaga@gmail.com>
2020-06-12 19:48:49 +0300

    Refactor

--- app/controllers/add-gist.js
+++ app/controllers/add-gist.js
@@ -1,4 +1,17 @@
 import Controller from '@ember/controller';
+import { action } from '@ember/object';
+import { inject as service } from '@ember/service';
+
 
 export default class AddGistController extends Controller {
+  @service gistsInfo;
+
+  @action
+  addFile (body, description, fileName) {
+    this.gistsInfo.addGist({
+      body,
+      description,
+      fileName,
+    });
+  }
 }
