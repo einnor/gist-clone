6eb026939337a00a4f447fffad58488b62eded5b
Ronnie Nyaga <ronnienyaga@gmail.com>
2020-06-12 19:38:29 +0300

    Hooking in the service

--- app/components/add-gist.js
+++ app/components/add-gist.js
@@ -1,10 +1,16 @@
 import Component from '@glimmer/component';
 import { action } from '@ember/object';
+import { inject as service } from '@glimmer/service';
 
 export default class AddGistComponent extends Component {
 
+  @service gistsInfo;
   @action
   addFile (body, description, fileName) {
-    
+    this.gistsInfo.addGist({
+      body,
+      description,
+      fileName,
+    });
   }
 }
