7d7e17bac8e4bc83a6b1486b56dab3f270acca03
Ronnie Nyaga <ronnienyaga@gmail.com>
2020-06-12 18:35:36 +0300

    Dummy data

--- app/controllers/add-gist.js
+++ app/controllers/add-gist.js
@@ -1,4 +1,22 @@
 import Controller from '@ember/controller';
 
 export default class AddGistController extends Controller {
+  gists = [
+    {
+      fileName: 'example.hbs',
+      description: 'Just an example',
+      body: '<p>An <strong>example</strong> for {{username}}</p>'
+    },
+    {
+      fileName: 'another-example.js',
+      description: 'Some JS',
+      body: `
+        import Component from '@glimmer/component'
+
+        export default class MyComponent extends Component {
+          variable = 'value';
+        }
+      `
+    },
+  ];
 }
