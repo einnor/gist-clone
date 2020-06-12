5266a9dd5a2ae82d833db5729b0d8038a27af964
Ronnie Nyaga <ronnienyaga@gmail.com>
2020-06-12 20:07:22 +0300

    Snap

--- app/controllers/index.js
+++ app/controllers/index.js
@@ -1,6 +1,22 @@
 import Controller from '@ember/controller';
 import { inject as service } from '@ember/service';
+import { action } from '@ember/object';
+import { tracked } from '@glimmer/tracking';
 
 export default class IndexController extends Controller {
   @service gistsInfo;
+
+  @tracked snapped = false;
+
+  get shownGists () {
+    if (this.snapped) {
+      return this.gistsInfo.gists.filter((item, index) => index % 2 === 0);
+    } else {
+      return this.gistsInfo.gists;
+    }
+  }
+
+  @action snap () {
+    this.snapped = !this.snapped;
+  }
 }
